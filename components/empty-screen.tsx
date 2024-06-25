7fd52fcf9dd9992b350e882ad847fe61aaca133d
Skanda Aryal <skandaaryal2055@gmail.com>
2024-06-25 09:59:44 +0545

    text changes

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -2,7 +2,7 @@ export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
-        <h1 className="text-lg font-semibold">Welcome to Capix AI Chatbot!</h1>
+        <h1 className="text-lg font-semibold">Welcome to Excel Sheet Analyzer!</h1>
         <p className="leading-normal text-muted-foreground">
           I’m here to assist you in analyzing your Excel spreadsheets. To get
           started, simply upload a spreadsheet file. You can ask me questions
